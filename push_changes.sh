8fe8f27a5ab00a8eceafe45bd8f8e9cfcbfa7b99
la-belle-femme <labellefemme2023@gmail.com>
2024-08-06 23:56:24 -0400

    created 2 masters and 3 worker nodes using terraform

--- push_changes.sh
+++ push_changes.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Initialize the git repository
-git init
+# Check the current branch
+current_branch=$(git branch --show-current)
 
-# Add the remote repository
-git remote add origin https://github.com/la-belle-femme/HA-K8-Cluster.git
+# If on master branch, create main branch if it doesn't exist
+if [ "$current_branch" == "master" ]; then
+  git branch -M main
+  git checkout main
+fi
 
-# Check the status of the working directory
-git status
+# Pull with --allow-unrelated-histories to merge unrelated histories
+git pull origin main --allow-unrelated-histories
 
 # Add all changes to staging
 git add .
@@ -18,20 +21,5 @@ git commit -m "created 2 masters and 3 worker nodes using terraform"
 # Push changes to the 'main' branch and set upstream
 git push --set-upstream origin main
 
-# Pull the latest changes from 'main'
-git pull origin main
-
-# Add all changes to staging
-git add .
-
-# Commit with the message passed as a parameter
-git commit -m "$1"
-
-# Push changes to the 'main' branch
-git push origin main
-
-# Additionally, push changes to the 'master' branch
-git push origin master
-
-# Set upstream for 'master' branch and push changes
-git push --set-upstream origin master
+# Additionally, push changes to the 'master' branch (if needed)
+git push origin master --set-upstream origin master
